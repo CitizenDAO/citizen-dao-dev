3f61ef6d934daf8fe638d78a3317c3e076353cc0
James Cunningham <j.a.cunningham@gmail.com>
2021-07-13 10:18:58 +0100

    Update deployment

--- deployments/truffle/migrations/2_deploy_token.js
+++ deployments/truffle/migrations/2_deploy_token.js
@@ -1,8 +1,7 @@
 const CitizenToken = artifacts.require("CitizenToken");
 
-module.exports = async function (deployer) {
-    deployer.deploy(CitizenToken, "Citizen Token Test", "CDAO", ["0x988d8D5Ca1063bD8c5fF7E0f4aFbEBEe4Ab8CFFE",
-                                                                 "0xAF90A0D9A2573eE956A278565ee8157934F3Fa9D",
-                                                                 "0x8e7DD33F31aDb21Ec2Eb3f819775B362F6Bb9B28"]);
-    cdao = await CitizenToken.deployed();
+module.exports = function (deployer) {
+    deployer.deploy(CitizenToken, "Citizen Token", "CDAO", ["0x988d8D5Ca1063bD8c5fF7E0f4aFbEBEe4Ab8CFFE",
+                                                            "0xAF90A0D9A2573eE956A278565ee8157934F3Fa9D",
+                                                            "0x8e7DD33F31aDb21Ec2Eb3f819775B362F6Bb9B28"]);
 }
